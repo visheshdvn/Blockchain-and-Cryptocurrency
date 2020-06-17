4615f3241604aadd021e4cd828ec3e22d50a1326
Vishesh <vishesh.dh1@gmail.com>
2020-06-18 03:09:39 +0530

    added sha256 hashing for hash

--- block.py
+++ block.py
@@ -1,22 +1,5 @@
 import time
-
-def mine_block(last_block, data):
-    """
-    Mine a block based on the given last_block and data.
-    """
-    timestamp = time.time_ns()
-    last_hash = last_block.hash
-    hash = f'{timestamp}-{last_hash}'
-    
-    return Block(timestamp, last_hash, hash, data)
-
-
-def genesis():
-    """
-    Generarte a genesis block
-    """
-    return Block(1, 'genesis_last_hash', 'genesis_hash', [])
-
+from crypto_hash import crypto_hash
 
 class Block():
     def __init__(self, timestamp, last_hash, hash, data):
@@ -31,12 +14,31 @@ class Block():
             f'timestamp: {self.timestamp}, '
             f'last_hash: {self.last_hash}, '
             f'hash: {self.hash}, '
-            f'data: {self.data}'
+            f'data: {self.data}) '
         )
+        
+    @staticmethod
+    def mine_block(last_block, data):
+        """
+        Mine a block based on the given last_block and data.
+        """
+        timestamp = time.time_ns()
+        last_hash = last_block.hash
+        hash = crypto_hash(timestamp, last_hash, hash, data)
+    
+        return Block(timestamp, last_hash, hash, data)
+    
+    @staticmethod
+    def genesis():
+        """
+        Generarte a genesis block
+        """
+        return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+
 
 def main():
-    genesis_block = genesis()
-    block = mine_block(genesis_block, 'foo')
+    genesis_block = Block.genesis()
+    block = Block.mine_block(genesis_block, 'foo')
     print(block)
 
 
